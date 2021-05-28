2934f4ef27d14eb0503092cad9e5a87181ba35aa
koharin <emma01818@gmail.com>
2021-05-28 20:50:50 +0900

    update /DatabaseProgramming

--- DatabaseProgramming/RDB_Schema/enroll.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE ENROLL(
-        s_id VARCHAR2(10),
-        c_id VARCHAR2(10),
-        c_id_no NUMBER(1),
-        e_year NUMBER(4) NOT NULL,
-        e_semester NUMBER(1) NOT NULL
-);
--- DatabaseProgramming/studentview.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE VIEW studentview(s_id, s_pwd, s_addr)
-AS
-select s_id, s_pwd, s_addr
-from students;
