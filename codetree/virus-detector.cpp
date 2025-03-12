41e6307dc599ba3939698b0283493850525bdcb8
koharin <dudu@sookmyung.ac.kr>
2025-03-12 14:00:41 +0900

    Update codetree/virus-detector.cpp

--- codetree/virus-detector.cpp
+++ codetree/virus-detector.cpp
@@ -6,7 +6,7 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     int n,LDR,MBR; cin >> n;
-    int answer=n;
+    long long answer=n; // 중요
     vector<int> REST(n,0);
     
     for(int i=0; i<n; i++) cin >> REST[i];
@@ -15,9 +15,9 @@ int main(){
     for(int i=0; i<n; i++){
         REST[i]-=LDR;
         if(REST[i]<=0) continue;
-        if(REST[i]%MBR==0) answer+=REST[i]/MBR;
-        else answer+=REST[i]/MBR + 1;
-        REST[i]/=MBR;
+        if(REST[i]%MBR==0) answer+=(REST[i]/MBR);
+        else answer+=(REST[i]/MBR) + 1;
+        //REST[i]/=MBR;
     }
     cout << answer << endl;
     return 0;
